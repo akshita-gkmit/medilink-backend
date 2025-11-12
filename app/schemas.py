4d5950d46a518939680b345622c1eb2e5d4f1d98
akshu-mishra <akshita@gkmit.co>
2025-11-13 00:27:47 +0530

    feat(schemas): add Pydantic models for registration and login

---
+++ app/schemas.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import date
+
+class RegisterRequest(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+    gender: str # "male" | "female" | "other"
+    dob: date
+    blood_group: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+    #login_as: str  # "patient" | "doctor" | "admin"
+
+    class Config:
+        orm_mode = True
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str
+    role: str
+
+    class Config:
+        orm_mode = True
