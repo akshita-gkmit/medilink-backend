5fe780f28af5b5bfbf2e5d60bbfad40ec0c851ce
akshu-mishra <akshita@gkmit.co>
2025-11-21 05:47:39 +0530

    make chnages in code

--- app/enums/status.py
+++ app/enums/status.py
@@ -1,4 +1,6 @@
-class SlotStatus(str, Enum):
+from app.enums.status import status
+
+class SlotStatus(str, status):
     AVAILABLE = "Available"
     PENDING = "Pending"
     CONFIRMED = "Confirmed"
